b0135cb1a20a5fe87569711f0de7e9670158bfe2
Adrian Faircloth <adrianspenser@gmail.com>
2024-05-05 19:11:28 -0400

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import test from "./components/test.vue"
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <head>
+    <title>Test Title</title>
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+  </head>
+<test />
 </template>
 
 <style scoped>
---
+++ src/components/test.vue
@@ -0,0 +1,13 @@
+<template>
+  <head>
+    <title>Test Title</title>
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+  </head>
+<h2><a href="home.html">back</a></h2>
+<input id="thisText" type="text" placeholder="Input search term"></input>
+<input id="thisButton" type="button" value="Click to search"></input>
+
+<h1>Search Results</h1>
+<p1 id="searchResults">
+</p1>
+</template>
